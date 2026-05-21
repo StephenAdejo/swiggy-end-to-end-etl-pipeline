95459dedf95a7dd1585f559243ff0fe7557f116f
Stephen Adejo <stephenadejo@outlook.com>
2026-05-21 06:31:04 +0100

    Add metadata header to analytics queries SQL file
    Added file header with metadata and purpose.

--- sql/06_analytics_queries.sql
+++ sql/06_analytics_queries.sql
@@ -1,1 +1,15 @@
-
+/******************************************************************************************
+ *  File Name: 06_analytics_queries.sql
+ *  Project:   Swiggy End-to-End ETL Pipeline
+ *  Purpose:   Business analytics queries and KPI calculations
+ *  
+ *  Author:    Stephen Adejo
+ *  Date:      May 2026
+ *  Version:   1.0
+ *  
+ *  Description:
+ *  Contains analytical queries to answer business questions such as 
+ *  popular food categories, restaurant performance, demand patterns, etc.
+ *  
+ *  Layer:     FACT
+ ******************************************************************************************/
